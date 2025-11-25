300dd3e9827a989d5911eaf67a4a59b2885e3a27
Jaan Hendrik Tomp <jaanh.tomp@gmail.com>
2025-11-25 18:27:45 +0200

    first success of migrating to vue, why oh why was this made

---
+++ src/components/footer.vue
@@ -0,0 +1,32 @@
+<template>
+<footer>
+    <div class = "bottom">
+        <h3> FOOTER </h3>
+    </div>
+</footer>
+</template>
+
+
+<script>
+</script>
+
+<style scoped>
+footer {
+    display: flex;
+    width: 100%;
+    background-color: white;
+    position: fixed;
+    bottom: 0;
+    padding-bottom: 10px;
+}
+
+.bottom {
+    display: flex;
+    padding: 10px;
+    width: 99%;
+    box-sizing: border-box;
+    background: #bfbfbf;
+    border-radius: 2vh;
+  justify-content: center;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+<header> <!-- Header code identical across pages except for the links -->
+    <div class = top>
+        <router-link to="/">Main</router-link> |
+        <router-link to="/login">Login</router-link> |
+    </div>
+</header>
+</template>
+
+
+<script>
+</script>
+
+<style scoped>
+.top {
+    display: flex;
+    padding: 10px;
+    width: 99%;
+    box-sizing: border-box;
+    background: #bfbfbf;
+    border-radius: 2vh;
+    align-self: auto;
+}
+
+header {
+    display: flex;
+    width: 100%;
+    background-color: white;
+    position: fixed;
+    top: 0;
+    padding-top: 10px;
+}
+</style>
