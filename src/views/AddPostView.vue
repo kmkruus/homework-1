644b167d9c8dea9525898f0c5f8b072a6d22da45
SirelLaud <laudsirel@gmail.com>
2025-12-16 23:40:57 +0200

    Added "AddPostView.vue"
    I for some reason am unable to connect it directly so I have to uplode the files manually for now

---
+++ src/views/AddPostView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <Header/>
+    <AddPost/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "/src/components/header.vue";
+import Footer from "/src/components/footer.vue";
+
+export default {
+  name: "AddPostView",
+  components: {
+    Header,
+    AddPost,
+    Footer
+  },
+};
+</script>
+
+<style scoped>
+body {
+    position: relative;
+}
+
+.post {
+    width: 80%;
+    position: relative;
+    padding: 10px;
+    margin: 10px auto;
+    border: 1px solid gray;
+}
+
+
+</style>
